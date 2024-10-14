232ccd4a12dc760b402d518a76e7c86a34391d7e
Callum Blejean <callumblejean@gmail.com>
2024-10-14 11:25:07 +0100

    build controller model app and the tests.

---
+++ models/topics.model.js
@@ -0,0 +1,10 @@
+const db = require("../db/connection");
+
+exports.fetchAllTopics = () => {
+  return db
+    .query(`SELECT * FROM topics;`)
+    .then(({ rows }) => rows)
+    .catch((error) => {
+      throw error;
+    });
+};
