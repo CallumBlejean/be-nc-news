545aa3db1b13077fced99ea82c8c5e99617463f5
Callum Blejean <callumblejean@gmail.com>
2024-11-04 09:54:40 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const cors = require('cors');
 const express = require("express");
 const app = express();
 const articlesRouter = require("./routes/articles.router");
@@ -6,6 +7,7 @@ const commentsRouter = require("./routes/comments.router");
 const usersRouter = require("./routes/users.router");
 const { getEndPoints } = require("./controllers/endPoints.controller");
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/api", getEndPoints);
